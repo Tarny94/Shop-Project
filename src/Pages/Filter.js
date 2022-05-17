aa1cb58782c1b12abc967247b7bdb5d25f48b4d4
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-05-17 18:54:26 +0300

    1. Setup App

---
+++ src/Pages/Filter.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+
+const Filter = () => {
+    return (
+        <div>
+            Border
+        </div>
+    )
+}
+
+export default Filter;
---
+++ src/Pages/Frontend.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+
+
+
+const Frontend = () => {
+    return (
+        <div className="containers">
+            Front page
+        </div>
+    )
+};
+
+
+export default Frontend;
