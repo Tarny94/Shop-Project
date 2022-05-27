701301f4726133f21a1aae12a79d01de0adc4ee6
Tarny <alextarnicer1994@gmail.com>
2022-05-27 13:24:41 +0300

    add submit button and styled, solve password hiddent

--- src/Component/Input.js
+++ src/Component/Input.js
@@ -1,13 +1,15 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
+import { type } from "@testing-library/user-event/dist/type";
 
-export default function Input({ label, value, onChange }) {
+export default function Input({ label, value, onChange, type }) {
   const handleEvent = (event) => {
     onChange(event.target.value);
   };
   return (
     <TextField
       label={label}
+      type={type}
       variant="outlined"
       value={value}
       onChange={handleEvent}
--- src/Pages/Authentification.js
+++ src/Pages/Authentification.js
@@ -1,11 +1,14 @@
+import { Password } from "@mui/icons-material";
 import React from "react";
 import Input from "../Component/Input";
+import Button from "../Component/Button";
 
 const Authentification = () => {
   return (
-    <div>
+    <div className="login-contained">
       <Input label="Username" />
-      <Input label="Password" />
+      <Input label="Password" type="password" />
+      <Button title="SUBMIT" />
     </div>
   );
 };
