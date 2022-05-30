7ccefef6e0b06f1689d04d725c5cf27215782ecc
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-05-30 16:19:15 +0300

    style page creation

--- src/Pages/ProductCreation.js
+++ src/Pages/ProductCreation.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "../styles/ProductCreation.scss";
 import { useNavigate } from "react-router-dom";
+import Input from "../Component/Input";
 
 const ProductCreation = () => {
   const navigate = useNavigate();
@@ -11,7 +12,48 @@ const ProductCreation = () => {
       navigate("/admin/login");
     }
   }, []);
-  return <div>ADMIN</div>;
+  return (
+    <div className="product-creation-contained">
+      <div>
+        <div className="product-creation-name creation">
+          <h1>Create Product</h1>
+        </div>
+        <div className="creation">
+          <Input type="text" label="Title" />
+        </div>
+        <div className="creation">
+          <Input label="Description" />
+        </div>
+        <div className="creation">
+          <Input label="Price $" type="number" />
+        </div>
+        <div className="creation">
+          <Input type="number" label="Discount Percentage %" />
+        </div>
+        <div className="creation">
+          <Input type="number" label="Rating" />
+        </div>
+        <div className="creation">
+          <Input type="number" label="Stock" />
+        </div>
+        <div className="creation">
+          <Input label="Brand" />
+        </div>
+        <div className="creation">
+          <Input label="Category" />
+        </div>
+        <div className="creation">
+          <Input label="Thumnail" />
+        </div>
+        <div className="creation">
+          <Input label="Images" />
+        </div>
+        <div className="creation">
+          <Input type="number" label="ID" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCreation;
